5b567e258f720df0011c004d5b5006ff273c852c
Terrell Ibanez <9453223+ibanezt@users.noreply.github.com>
2020-04-10 10:33:00 -0400

    Update instructions / documentation

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# standout
+# Standout
+Standout is a text editor that allows you to format (Bold / Italic) your profile text with Unicode.
+
+## How It Works
+By converting text to Unicode, we can use Bold and Italic even if the site / app does not support it directly.
+
+The editor is written in React / TypeScript and Fluent UI.
+
+## Try It Out
+https://standout.terrell.dev
+
+## How to Use
+Type/copy profile text into the editor box, highlight text and click a button to apply the corresponding format. When finished, copy it to your profile.
+
+
+
+## Limitations
+The editor supports Bold for letters / numbers, and Italic for letters.
+
+NOTE: Any text that is styled (Bold or Italic) is NOT searchable!
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import { Link, Text, Stack, TextField, } from 'office-ui-fabric-react';
+import { Link, Text, Stack, TextField } from 'office-ui-fabric-react';
 import { CommandBar, ICommandBarItemProps } from 'office-ui-fabric-react/lib/CommandBar';
 import { Depths } from '@uifabric/fluent-theme/lib/fluent/FluentDepths';
 import { initializeIcons } from '@uifabric/icons';
 import { applyStyle } from './Style'
+
 export const App: React.FunctionComponent = () => {
   const [editorText, setEditorText] = useState('');
-
   function applyEditorStyle(style: string) {
     let editor = document.getElementById('editor') as HTMLTextAreaElement;
     let textSelected = '';
@@ -51,6 +51,16 @@ export const App: React.FunctionComponent = () => {
         applyEditorStyle('regular');
       },
     },
+    /*
+    {
+      key: 'help',
+      text: 'Help',
+      iconProps: { iconName: 'Help' },
+      onClick: () => {
+        console.log('Help');
+      },
+    },
+    */
   ];
 
   return (
@@ -58,36 +68,45 @@ export const App: React.FunctionComponent = () => {
       horizontal={false}
       verticalAlign='start'
       horizontalAlign='center'
-      verticalFill
+      padding='10px'
     >
       <Stack
-        verticalFill
-        padding='60px'
-        style={{ width: '100%', maxWidth: 800, boxShadow: Depths.depth4 }}
+        padding='30px'
+        style={{ width: '100%', maxWidth: 840, boxShadow: Depths.depth8 }}
       >
         <Stack style={{ textAlign: 'center' }}>
           <Text variant='mega'>Standout</Text>
-        </Stack>
+          <Text>Type profile text into editor box, highlight text to bold or italicize and click the button to apply.</Text>
 
+        </Stack>
         <CommandBar
           items={_items}
           style={{ boxShadow: Depths.depth4 }}
         />
-        <EditField label="Editor"
-          text={editorText}
-          setText={setEditorText}
-        />
-        <Stack horizontalAlign='center'>
-          <Text>Designed by <Link target='_blank' href='https://github.com/ibnzterrell/'>Terrell Ibanez</Link></Text>
+        < Stack
+
+        >
+          <EditField label="Editor"
+            text={editorText}
+            setText={setEditorText}
+          />
+
+        </Stack>
+
+
+        <Stack
+          horizontalAlign='center'>
+          <Stack
+            style={{ height: 20 }}
+          >
+          </Stack>
+          <Text>Designed by <Link target='_blank' href='https://www.linkedin.com/in/ibnzterrell/'>Terrell Ibanez</Link></Text>
           <Link target='_blank' href='https://github.com/ibnzterrell/standout/'>Source Code on Github</Link>
         </Stack>
       </Stack>
     </Stack >
   );
 };
-
-
-
 
 type EditFieldProps = {
   label: string
@@ -98,7 +117,7 @@ export const EditField = ({ label, text, setText }: EditFieldProps) => (
   <TextField
     label={label}
     multiline
-    rows={5}
+    rows={10}
     autoAdjustHeight
     resizable={false}
     value={text}
